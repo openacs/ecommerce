f6e396dc9ab493ebb9eb2c73eda70866b64a1b80
Bart Teeuwisse <bart.teeuwisse@thecodemill.biz>
2002-01-06 22:53:15 +0000

    Enabled the ecommerce products to the searched by the OpenFTS search.

---
+++ tcl/ec-product-sc-procs.tcl
@@ -0,0 +1,40 @@
+ad_proc ec_products__datasource {
+    object_id
+} {
+    @author Gilbert Wong
+    @author Bart Teeuwisse (bart.teeuwisse@7-sisters.com)
+} {
+    db_0or1row ec_products_datasource {
+        select e.product_id as object_id,
+               e.product_name as title,
+               e.detailed_description as content,
+               'text/plain' as mime,
+               search_keywords as keywords,
+               'text' as storage_type
+        from ec_products e
+        where e.product_id = :object_id
+    } -column_array datasource
+
+    return [array get datasource]
+}
+
+
+ad_proc ec_products__url {
+    object_id
+} {
+    @author Gilbert Wong
+} {
+
+    set package_id [apm_package_id_from_key ecommerce]
+    db_1row get_url_stub "
+        select site_node__url(node_id) as url_stub
+        from site_nodes
+        where object_id=:package_id
+    "
+
+    set url "${url_stub}/product?product_id=$object_id"
+
+    return $url
+}
+
+
