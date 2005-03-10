3becc71d7d8cd97dd716693d212d15bdf99e6dae
Torben Brosten <torben@dekka.net>
2005-03-10 10:26:10 +0000

    repositioned loop that sets empty values to nonexistent vars so that it gets run before other code. fixes error about current_location nonexistent

--- www/toolbar.tcl
+++ www/toolbar.tcl
@@ -11,17 +11,17 @@
 #     @cvs-id $Id$
 #     @creation-date September 2002
 
-# we'll show a search widget at the top if there are categories to search in
-if { ![empty_string_p [db_string get_check_of_categories "select 1 from dual where exists (select 1 from ec_categories)" -default ""]] } {
-    # Create a context aware search widget to search for products.
-
 # Create empty values for each optional parameter that has not been passed from a master template.
 foreach parameter {combocategory_id category_id subcategory_id search_text current_location} {
     if {![info exists $parameter]} {
 	set $parameter {}
     }
 }
-    
+
+# we'll show a search widget at the top if there are categories to search in
+if { ![empty_string_p [db_string get_check_of_categories "select 1 from dual where exists (select 1 from ec_categories)" -default ""]] } {
+    # Create a context aware search widget to search for products.
+
     # Decode the combo of category and subcategory ids
     if { ![empty_string_p $combocategory_id] } {
         set category_id [lindex [split $combocategory_id "|"] 0]
