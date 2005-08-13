786e13ec6930b30b9a1e30e62261356703b5ad11
Torben Brosten <torben@dekka.net>
2005-08-13 09:35:02 +0000

    standardized oracle query, adding postgresql equivalent, closes bug 2491

--- www/admin/customer-service/gift-certificates-postgresql.xql
+++ www/admin/customer-service/gift-certificates-postgresql.xql
@@ -3,11 +3,35 @@
 <queryset>
    <rdbms><type>postgresql</type><version>7.1</version></rdbms>
 
+
 <fullquery name="get_pretty_price">      
       <querytext>
       select ec_gift_certificate_balance(:user_id) 
       </querytext>
 </fullquery>
 
+<fullquery name="get_gift_certificates_for_user">
+    <querytext>
+        select c.*, 
+               i.first_names || ' ' || i.last_name as issuer, 
+               i.user_id as issuer_user_id, 
+               p.first_names || ' ' || p.last_name as purchaser, 
+               p.user_id as purchaser_user_id, 
+               gift_certificate_amount_left(c.gift_certificate_id) as amount_left, 
+               (now() - expires) <= 0 as expired_p, 
+               v.first_names as voided_by_first_names, v.last_name as voided_by_last_name
+               gift_certificate_amount_left(gift_certificate_id) = 0 as amount_left_is_zero,
+               gift_certificate_state = 'void' as state_is_void
+        from ec_gift_certificates c, 
+             cc_users i, 
+             cc_users p, 
+             cc_users v
+        where c.issued_by=i.user_id
+        and c.purchased_by=p.user_id
+        and c.voided_by=v.user_id
+        and c.user_id=:user_id
+        order by expired_p, amount_left_is_zero, state_is_void, gift_certificate_id
+    </querytext>  
+</queryset>
  
 </queryset>
--- www/admin/customer-service/gift-certificates.tcl
+++ www/admin/customer-service/gift-certificates.tcl
@@ -50,13 +50,22 @@ append doc_body "<b>Customer:</b> <a href=\"[ec_acs_admin_url]users/one?user_id=
 
 "
 
-set sql "select c.*, i.first_names || ' ' || i.last_name as issuer, i.user_id as issuer_user_id, p.first_names || ' ' || p.last_name as purchaser, p.user_id as purchaser_user_id, gift_certificate_amount_left(c.gift_certificate_id) as amount_left, decode(sign(sysdate-expires),1,'t',0,'t','f') as expired_p, v.first_names as voided_by_first_names, v.last_name as voided_by_last_name
-from ec_gift_certificates c, cc_users i, cc_users p, cc_users v
-where c.issued_by=i.user_id(+)
-and c.purchased_by=p.user_id(+)
-and c.voided_by=v.user_id(+)
-and c.user_id=:user_id
-order by expired_p, decode(amount_left,0,1,0), decode(gift_certificate_state,'void',1,0), gift_certificate_id"
+set sql "select c.*, 
+            i.first_names || ' ' || i.last_name as issuer, 
+            i.user_id as issuer_user_id, 
+            p.first_names || ' ' || p.last_name as purchaser, 
+            p.user_id as purchaser_user_id, 
+            gift_certificate_amount_left(c.gift_certificate_id) as amount_left, 
+            decode(sign(sysdate-expires),1,'t',0,'t','f') as expired_p, 
+            v.first_names as voided_by_first_names, v.last_name as voided_by_last_name
+            decode(amount_left,0,'t','f') as amount_left_is_zero,
+            decode(gift_certificate_state,'void','t','f') as state_is_void
+        from ec_gift_certificates c, cc_users i, cc_users p, cc_users v
+        where c.issued_by=i.user_id(+)
+            and c.purchased_by=p.user_id(+)
+            and c.voided_by=v.user_id(+)
+            and c.user_id=:user_id
+        order by expired_p, amount_left_is_zero, state_is_void, gift_certificate_id"
 
 set gift_certificate_counter 0
 set current_printed 0
