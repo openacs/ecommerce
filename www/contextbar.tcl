160ace0d83dfb6a6676d1b1af93d7d2292e9d116
Mark Aufflick <mark@aufflick.com>
2008-04-28 13:01:33 +0000

    stop bumping admins to login page for timed-out logins

--- www/contextbar.tcl
+++ www/contextbar.tcl
@@ -23,7 +23,7 @@ if {[empty_string_p $context_addition]} {
 
 # Check for admin rights to this (the ecommerce) package.
 
-set ec_admin_p [ad_permission_p [ad_conn package_id] admin]
+set ec_admin_p [permission::permission_p -no_login -object_id [ad_conn package_id] -privilege admin]
 if {$ec_admin_p} {
     set ec_admin_link "[apm_package_url_from_id [ad_conn package_id]]admin/"
 }
