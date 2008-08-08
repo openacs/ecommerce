8ba6f3554b68db83e7edf72b7925c6c9ae08a3b0
Torben Brosten <torben@dekka.net>
2008-08-08 12:23:47 +0000

    adding more flexible templating, more consistent with forum templating

---
+++ lib/contextbar.adp
@@ -0,0 +1,1 @@
+<p><if @ec_admin_p@>[&nbsp;<a href="@ec_admin_link@" title="Administer @ec_system_name;noquote@">Administer</a>&nbsp;] </if>[&nbsp;<a href="@cart_link@" title="Your Shopping Cart">View Your Shopping Cart</a>&nbsp;] </p>
---
+++ lib/contextbar.tcl
@@ -0,0 +1,17 @@
+#     Add a [ Administer ] link to the template
+
+
+
+# Check for admin rights to this (the ecommerce) package.
+
+set ec_admin_p [permission::permission_p -no_login -object_id [ad_conn package_id] -privilege admin]
+if {$ec_admin_p} {
+    set ec_admin_link "[ec_url]admin/"
+}
+
+set cart_link "[ec_url]shopping-cart"
+
+# Get the name of the ecommerce package
+
+set ec_system_name [ec_system_name]
+
