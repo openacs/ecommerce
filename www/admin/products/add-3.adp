8930e14d0406a67ad1dbb2bfe8e7c325414c5a75
Torben Brosten <torben@dekka.net>
2008-08-13 12:16:57 +0000

    updating templating for add-3.adp/tcl

---
+++ www/admin/products/add-3.adp
@@ -0,0 +1,170 @@
+<master>
+  <property name="doc(title)">@title;noquote@</property>
+  <property name="context">@context;noquote@</property>
+<h2>@title@</h2>
+
+<h3>Please confirm that the information below is correct:</h3>
+<form method=post action=add-4>
+<center>
+<input type=submit value="Confirm">
+</center>
+<blockquote>
+@linked_thumbnail;noquote@
+<table noborder>
+<tr><td align="right">
+Product Name:
+</td>
+<td>
+@product_name;noquote@
+</td>
+</tr>
+<tr><td align="right">
+SKU:
+</td>
+<td>
+@sku@
+</td>
+</tr>
+<tr><td align="right">
+Categorization:
+</td>
+<td>
+@categorization_html;noquote@
+</td>
+</tr>
+<if @multiple_retailers_p@ true>
+
+
+</if><else>
+<tr>
+    <td align="right">
+    Stock Status:
+    </td>
+    <td>@stock_status_html;noquote@
+</td>
+    </tr>
+<tr>
+    <td align="right">
+    Regular Price:
+    </td>
+    <td>
+    @price_html@
+    </td>
+    </tr>
+    <tr>
+    <td align="right">
+    Shipping Price
+    </td>
+    <td>
+@shipping_html@
+    </td>
+    </tr>
+    <tr>
+    <td align="right">
+    Shipping - Additional
+    </td>
+    <td>
+
+@shipping_additional_html@
+    </td>
+    </tr>
+
+</else>
+<tr><td align="right">
+One-Line Description:
+</td>
+<td>
+@one_line_description_html;noquote@
+</td>
+</tr>
+<tr><td align="right">
+Additional Descriptive Text:
+</td>
+<td>
+@detailed_description_html;noquote@
+</td>
+</tr>
+<tr><td align="right">
+Search Keywords:
+</td>
+<td>
+@search_keywords_html@
+</td>
+</tr>
+<tr><td align="right">
+Color Choices:
+</td>
+<td>
+@color_list_html;noquote@
+</td>
+</tr>
+<tr><td align="right">
+Size Choices:
+</td>
+<td>
+@size_list_html;noquote@
+</td>
+</tr>
+<tr><td align="right">
+Style Choices:
+</td>
+<td>
+@style_list_html;noquote@
+</td>
+</tr>
+<tr><td align="right">
+Email On Purchase:
+</td>
+<td>
+@email_on_purchase_list_html@
+</td>
+</tr>
+<tr><td align="right">
+URL:
+</td>
+<td>
+@url_html@
+</td>
+</tr>
+<tr><td align="right">
+Is this product shippable?
+</td>
+<td>
+@no_shipping_avail_p_html@
+</td>
+</tr>
+<tr><td align="right">
+Display this product when user does a search?
+</td>
+<td>
+@present_p_html@
+</td>
+</tr>
+<tr><td align="right">
+Weight
+</td>
+<td>
+@weight_html@
+</td>
+</tr>
+@custom_product_fields_select_html;noquote@
+<tr><td align="right">
+Template
+</td>
+<td>
+@template_html;noquote@
+</td>
+</tr>
+</table>
+</blockquote>
+<p>
+@export_form_vars_html;noquote@
+
+@custom_fields_select_html;noquote@
+
+@user_class_select_html;noquote@
+<center>
+<input type=submit value="Confirm">
+</center>
+</form>
+
--- www/admin/products/add-3.tcl
+++ www/admin/products/add-3.tcl
@@ -51,265 +51,84 @@ ad_require_permission [ad_conn package_id] admin
 # and all active custom fields (except ones that are boolean and weren't filled in)
 # and price$user_class_id for all the user classes
 # category_id_list, subcategory_id_list, subsubcategory_id_list
+set title "Confirm New Product"
+set context [list $title]
 
-doc_body_append "[ad_admin_header "Confirm New Product"]
 
-<h2>Confirm New Product</h2>
+set currency [parameter::get -package_id [ec_id] -parameter Currency]
+set multiple_retailers_p [parameter::get -package_id [ec_id] -parameter MultipleRetailersPerProductP -default f]
 
-[ad_context_bar [list "../" "Ecommerce([ec_system_name])"] [list "index" "Products"] "Add Product"]
+set categorization_html [ec_category_subcategory_and_subsubcategory_display $category_id_list $subcategory_id_list $subsubcategory_id_list]
 
-<hr>
-<h3>Please confirm that the information below is correct:</h3>
-"
-set currency [ad_parameter -package_id [ec_id] Currency ecommerce]
-set multiple_retailers_p [ad_parameter -package_id [ec_id] MultipleRetailersPerProductP ecommerce]
-
-doc_body_append "<form method=post action=add-4>
-<center>
-<input type=submit value=\"Confirm\">
-</center>
-<blockquote>
-$linked_thumbnail
-<table noborder>
-<tr>
-<td>
-Product Name:
-</td>
-<td>
-$product_name
-</td>
-</tr>
-<tr>
-<td>
-SKU:
-</td>
-<td>
-$sku
-</td>
-</tr>
-<tr>
-<td>
-Categorization:
-</td>
-<td>
-[ec_category_subcategory_and_subsubcategory_display $category_id_list $subcategory_id_list $subsubcategory_id_list]
-</td>
-</tr>
-"
 if { !$multiple_retailers_p } {
-    doc_body_append "<tr>
-    <td>
-    Stock Status:
-    </td>
-    <td>
-    "
     if { ![empty_string_p $stock_status] } {
-	doc_body_append [util_memoize "ad_parameter -package_id [ec_id] \"StockMessage[string toupper $stock_status]\"" [ec_cache_refresh]]
+        set stock_status_html [util_memoize "ad_parameter -package_id [ec_id] \"StockMessage[string toupper $stock_status]\"" [ec_cache_refresh]]
     } else {
-	doc_body_append [ec_message_if_null $stock_status]
+        set stock_status_html [ec_message_if_null $stock_status]
     }
+    set price_html [ec_message_if_null [ec_pretty_price $price $currency]]
+    set shipping_html [ec_message_if_null [ec_pretty_price $shipping $currency]]
+    set shipping_additional_html [ec_message_if_null [ec_pretty_price $shipping_additional $currency]]
+}
 
-    doc_body_append "</td>
-    </tr>
-    "
-}
-doc_body_append "<tr>
-<td>
-One-Line Description:
-</td>
-<td>
-[ec_message_if_null $one_line_description]
-</td>
-</tr>
-<tr>
-<td>
-Additional Descriptive Text:
-</td>
-<td>
-[ec_display_as_html [ec_message_if_null $detailed_description]]
-</td>
-</tr>
-<tr>
-<td>
-Search Keywords:
-</td>
-<td>
-[ec_message_if_null $search_keywords]
-</td>
-</tr>
-<tr>
-<td>
-Color Choices:
-</td>
-<td>
-[ec_message_if_null $color_list]
-</td>
-</tr>
-<tr>
-<td>
-Size Choices:
-</td>
-<td>
-[ec_message_if_null $size_list]
-</td>
-</tr>
-<tr>
-<td>
-Style Choices:
-</td>
-<td>
-[ec_message_if_null $style_list]
-</td>
-</tr>
-<tr>
-<td>
-Email On Purchase:
-</td>
-<td>
-[ec_message_if_null $email_on_purchase_list]
-</td>
-</tr>
-<tr>
-<td>
-URL:
-</td>
-<td>
-[ec_message_if_null $url]
-</td>
-</tr>
-"
-if { !$multiple_retailers_p } {
-    doc_body_append "<tr>
-    <td>
-    Regular Price:
-    </td>
-    <td>
-    [ec_message_if_null [ec_pretty_price $price $currency]]
-    </td>
-    </tr>
-    "
-}
-doc_body_append "<tr>
-<td>
-Is this product shippable?
-</td>
-<td>
-[ec_message_if_null [ec_PrettyBoolean [ec_decode $no_shipping_avail_p "t" "f" "f" "t"]]]
-</td>
-</tr>
-<tr>
-<td>
-Display this product when user does a search?
-</td>
-<td>
-[ec_message_if_null [ec_PrettyBoolean $present_p]]
-</td>
-</tr>
-"
-if { !$multiple_retailers_p } {
-    doc_body_append "<tr>
-    <td>
-    Shipping Price
-    </td>
-    <td>
-    [ec_message_if_null [ec_pretty_price $shipping $currency]]
-    </td>
-    </tr>
-    <tr>
-    <td>
-    Shipping - Additional
-    </td>
-    <td>
-    [ec_message_if_null [ec_pretty_price $shipping_additional $currency]]
-    </td>
-    </tr>
-    "
-}
-doc_body_append "<tr>
-<td>
-Weight
-</td>
-<td>
-[ec_message_if_null $weight] [ec_decode $weight "" "" [ad_parameter -package_id [ec_id] WeightUnits ecommerce]]
-</td>
-</tr>
-"
-if { !$multiple_retailers_p } {
-  db_foreach user_class_select "select user_class_id, user_class_name from ec_user_classes order by user_class_name" {
+set user_class_select_html ""
+db_foreach user_class_select "select user_class_id, user_class_name from ec_user_classes order by user_class_name" {
     if { [info exists user_class_prices($user_class_id)] } {
-      doc_body_append "
-      <tr>
-      <td>
-      $user_class_name Price:
-      </td>
-      <td>
+        append user_class_select_html "
+      <tr><td align=\"right\">${user_class_name} Price: </td><td>
       [ec_message_if_null [ec_pretty_price [set user_class_prices($user_class_id)] $currency]]
-      </td>
-      </tr>
-      "
+      </td></tr>"
     }
-  }
 }
+set one_line_description_html [ec_message_if_null $one_line_description]
 
+set detailed_description_html [ec_display_as_html [ec_message_if_null $detailed_description]]
+set search_keywords_html [ec_message_if_null $search_keywords]
+set color_list_html [ec_message_if_null $color_list]
+set size_list_html [ec_message_if_null $size_list]
+set style_list_html [ec_message_if_null $style_list]
+set email_on_purchase_list_html [ec_message_if_null $email_on_purchase_list]
+set url_html [ec_message_if_null $url]
+set no_shipping_avail_p_html [ec_message_if_null [ec_PrettyBoolean [ec_decode $no_shipping_avail_p "t" "f" "f" "t"]]]
+set present_p_html [ec_message_if_null [ec_PrettyBoolean $present_p]]
+    
+set weight_html "[ec_message_if_null $weight] [ec_decode $weight "" "" [parameter::get -package_id [ec_id] -parameter WeightUnits -default lbs]]"
+
+set custom_product_fields_select_html ""
 db_foreach custom_fields_select {
-  select field_identifier, field_name, column_type
-  from ec_custom_product_fields
-  where active_p = 't'
-} {
-  if { [info exists ec_custom_fields($field_identifier)] } {
-    doc_body_append "
-    <tr>
-    <td>
-    $field_name
-    </td>
-    <td>
-    "
-    if { $column_type == "char(1)" } {
-      doc_body_append "[ec_message_if_null [ec_PrettyBoolean $ec_custom_fields($field_identifier)]]\n"
-    } elseif { $column_type == "date" } {
-      doc_body_append "[ec_message_if_null [util_AnsiDatetoPrettyDate $ec_custom_fields($field_identifier)]]\n"
-    } else {
-      doc_body_append "[ec_display_as_html [ec_message_if_null $ec_custom_fields($field_identifier)]]\n"
-    }
-    doc_body_append "</td>
-    </tr>
-    "
-  }
-}
-
-doc_body_append "<tr>
-<td>
-Template
-</td>
-<td>
-[ec_message_if_null [db_string template_name_select "select template_name from ec_templates where template_id=:template_id" -default "" ]]
-</td>
-</tr>
-</table>
-</blockquote>
-<p>
-[export_form_vars product_name sku category_id_list subcategory_id_list subsubcategory_id_list one_line_description detailed_description color_list size_list style_list email_on_purchase_list search_keywords url price no_shipping_avail_p present_p available_date shipping shipping_additional weight template_id product_id dirname stock_status]
-"
-
-# also need to export custom field values
-db_foreach custom_fields_select {
-  select field_identifier 
-  from ec_custom_product_fields 
-  where active_p='t'
+    select field_identifier, field_name, column_type
+    from ec_custom_product_fields
+    where active_p = 't'
 } {
     if { [info exists ec_custom_fields($field_identifier)] } {
-      doc_body_append "<input type=hidden name=\"ec_custom_fields.$field_identifier\" value=\"[ad_quotehtml $ec_custom_fields($field_identifier)]\">\n"
+        append custom_product_fields_select_html "<tr><td align=\"right\">$field_name</td><td>"
+        if { $column_type == "char(1)" } {
+            append custom_product_fields_select_html "[ec_message_if_null [ec_PrettyBoolean $ec_custom_fields($field_identifier)]]\n"
+        } elseif { $column_type == "date" } {
+            append custom_product_fields_select_html "[ec_message_if_null [util_AnsiDatetoPrettyDate $ec_custom_fields($field_identifier)]]\n"
+        } else {
+            append custom_product_fields_select_html "[ec_display_as_html [ec_message_if_null $ec_custom_fields($field_identifier)]]\n"
+        }
+        append custom_product_fields_select_html "</td></tr>"
     }
 }
 
+set template_html [ec_message_if_null [db_string template_name_select "select template_name from ec_templates where template_id=:template_id" -default "" ]]
+set export_form_vars_html [export_form_vars product_name sku category_id_list subcategory_id_list subsubcategory_id_list one_line_description detailed_description color_list size_list style_list email_on_purchase_list search_keywords url price no_shipping_avail_p present_p available_date shipping shipping_additional weight template_id product_id dirname stock_status]
+
+# also need to export custom field values
+set custom_fields_select_html ""
+db_foreach custom_fields_select {
+    select field_identifier 
+    from ec_custom_product_fields 
+    where active_p='t'
+} {
+    if { [info exists ec_custom_fields($field_identifier)] } {
+        append custom_fields_select_html "<input type=hidden name=\"ec_custom_fields.$field_identifier\" value=\"[ad_quotehtml $ec_custom_fields($field_identifier)]\">\n"
+    }
+}
+set user_class_select_html ""
 foreach user_class_id [db_list user_class_select "select user_class_id from ec_user_classes"] {
-  doc_body_append "<input type=hidden name=\"user_class_prices.$user_class_id\" value=\"$user_class_prices($user_class_id)\">"
+    append user_class_select_html "<input type=hidden name=\"user_class_prices.$user_class_id\" value=\"$user_class_prices($user_class_id)\">"
 }
 
-doc_body_append "<center>
-<input type=submit value=\"Confirm\">
-</center>
-</form>
-
-[ad_admin_footer]
-"
