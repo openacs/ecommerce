ac3f2c068e726904967ca75b08a594a17e3797c7
Torben Brosten <torben@dekka.net>
2008-08-13 11:10:27 +0000

    updating templating for add.tcl/add.adp

---
+++ www/admin/products/add.adp
@@ -0,0 +1,148 @@
+<master>
+  <property name="doc(title)">@title;noquote@</property>
+  <property name="context">@context;noquote@</property>
+<h2>@title@</h2>
+
+<p>All fields are optional except Product Name.</p>
+
+<form enctype="multipart/form-data" method="post" action="add-2">
+<table>
+<tr>
+<td>Product Name</td>
+<td colspan=2><input type=text name=product_name size=30></td>
+</tr>
+<tr>
+<td>SKU</td>
+<td><input type=text name=sku size=10></td>
+<td>It's not necessary to include a SKU because the system generates its own
+internal product_id to uniquely distinguish products.</td>
+</tr>
+<tr>
+<td>Product Category</td>
+<td>@product_category_html;noquote@</td>
+<td>Choose as many categories as you like.  The product will
+be displayed on the web site in each of the categories you select.</td>
+</tr>
+<if @multiple_retailers_p@ false>
+<tr>
+    <td>Regular Price</td>
+    <td><input type=text size=6 name=price></td>
+    <td>All prices are in @currency@.  The price should
+    be written as a decimal number (no special characters like \$).
+</tr>
+<tr>
+    <td>Stock Status</td>
+    <td colspan=2>@stock_status_html;noquote@</td>
+    </tr>
+
+<tr>
+    <td>Shipping Price</td>
+    <td><input type=text size=6 name=shipping></td>
+    <td rowspan=3 valign=top>The "Shipping Price", "Shipping Price - Additional", and "Weight" fields
+    may or may not be applicable, depending on the 
+    <a href="../shipping-costs/index">shipping rules</a> you have set up for
+    your ecommerce system.</td>
+</tr>
+<tr>
+    <td>Shipping Price - Additional per item if ordering more than 1 (leave blank if same as Shipping Price above)</td>
+    <td><input type=text size=6 name=shipping_additional></td>
+</tr>
+
+</if><else>
+@stock_status_html;noquote@
+@price_html;noquote@
+</else>
+
+<tr>
+<td>One-Line Description</td>
+<td colspan=2><input type=text name=one_line_description size=60></td>
+</tr>
+<tr>
+<td>Additional Descriptive Text</td>
+<td colspan=2><textarea wrap rows=6 cols=60 name=detailed_description></textarea></td>
+</tr>
+<tr>
+<td><p>Search Keywords</p>
+<p>Data from Product's Name, One Line Description, SKU and Other Detailed Description 
+are automatically included in product searches. No need to repeat as keywords.</p></td>
+<td colspan=2><textarea wrap rows=2 cols=60 name=search_keywords></textarea></td>
+</tr>
+<tr>
+<td>Picture</td>
+<td><input type=file size=10 name=upload_file></td>
+<td>This picture (.gif or .jpg format) can be as large as you like.  A thumbnail will be automatically generated.  Note that file uploading doesn't work with Internet Explorer 3.0.</td>
+</tr>
+<tr>
+<td>Color Choices</td>
+<td><input type=text name=color_list size=40></td>
+<td>This should be a comma-separated list of colors the user is allowed to choose from
+when ordering.  If there are no choices, leave this blank.</td>
+</tr>
+<tr>
+<td>Size Choices</td>
+<td><input type=text name=size_list size=40></td>
+<td>This should be a comma-separated list of sizes the user is allowed to choose from
+when ordering.  If there are no choices, leave this blank.</td>
+</tr>
+<tr>
+<td>Style Choices</td>
+<td><input type=text name=style_list size=40></td>
+<td>This should be a comma-separated list of styles the user is allowed to choose from
+when ordering.  If there are no choices, leave this blank.</td>
+</tr>
+<tr>
+<td>Email on Purchase</td>
+<td><input type=text name=email_on_purchase_list size=40></td>
+<td>This should be a comma-separated list of recipients to notify when a purchase is made. If you do not wish email to be sent, leave this blank.</td>
+</tr>
+<tr>
+<td>URL where the consumer can get more info on the product</td>
+<td colspan=2><input type=text name=url size=50 value="http://"></td>
+</tr>
+
+<tr>
+<td>Is this product shippable?</td>
+<td><input type=radio name=no_shipping_avail_p value="f" checked>Yes
+&nbsp;&nbsp;
+<input type=radio name=no_shipping_avail_p value="t">No
+</td>
+<td>You might choose "No" if this product is actually a service.</td>
+</tr>
+<tr>
+<td>Should this product be displayed when the user does a search?</td>
+<td><input type=radio name=present_p value="t" checked>Yes
+&nbsp;&nbsp;
+<input type=radio name=present_p value="f">No
+</td>
+<td>You might choose "No" if this product is part of a series.</td>
+</tr>
+<tr>
+<td>When does this product become available for purchase?</td>
+<td>@available_date_html;noquote@</td>
+</tr>
+<tr>
+<td>Weight (@weight_units@)</td>
+<td><input type=text size=3 name=weight></td>
+</tr>
+</table>
+
+<if @n_user_classes@ true>
+<h3>Special Prices for User Classes</h3>
+    <br/>
+    <table noborder>
+@user_classes_html;noquote@
+    </table>
+</if>
+
+<if @n_custom_product_fields@ gt 0>
+<h3>Custom Fields</h3>
+    <br/>
+    <table noborder>
+@custom_product_fields_html;noquote@
+    </table>
+</if>
+
+<center>
+<input type=submit value="Continue">
+</center>
+</form>
--- www/admin/products/add.tcl
+++ www/admin/products/add.tcl
@@ -14,212 +14,60 @@ ad_page_contract {
 
 ad_require_permission [ad_conn package_id] admin
 
-doc_body_append "[ad_admin_header "Add a Product"]
+set title "Add a Product"
+set context [list $title]
 
-<h2>Add a Product</h2>
+set currency [parameter::get -package_id [ec_id] -parameter Currency]
+set multiple_retailers_p [parameter::get -package_id [ec_id] -parameter MultipleRetailersPerProductP -default 0]
+set weight_units [parameter::get -package_id [ec_id] -parameter WeightUnits -default lbs] 
 
-[ad_context_bar [list "../" "Ecommerce([ec_system_name])"] [list "index" "Products"] "Add Product"]
-
-<hr>
-
-All fields are optional except Product Name.
-<p>
-"
-set multiple_retailers_p [ad_parameter -package_id [ec_id] MultipleRetailersPerProductP ecommerce]
-
-
-
-doc_body_append "<form enctype=multipart/form-data method=post action=add-2>
-<table>
-<tr>
-<td>Product Name</td>
-<td colspan=2><input type=text name=product_name size=30></td>
-</tr>
-<tr>
-<td>SKU</td>
-<td><input type=text name=sku size=10></td>
-<td>It's not necessary to include a SKU because the system generates its own
-internal product_id to uniquely distinguish products.</td>
-</tr>
-<tr>
-<td>Product Category</td>
-<td>[ec_category_widget t]</td>
-<td>Choose as many categories as you like.  The product will
-be displayed on the web site in each of the categories you select.</td>
-</tr>
-"
-if { !$multiple_retailers_p } {
-    doc_body_append "<tr>
-    <td>Stock Status</td>
-    <td colspan=2>[ec_stock_status_widget]</td>
-    </tr>
-    "
+if { $multiple_retailers_p } {
+    set stock_status_html [ec_hidden_input stock_status ""]
+    set price_html [ec_hidden_input price ""]
+    set shipping_html [ec_hidden_input shipping ""]
+    set shipping_additional_html [ec_hidden_input shipping_additional ""]
 } else {
-    doc_body_append "[ec_hidden_input stock_status ""]\n"
-}
-doc_body_append "<tr>
-<td>One-Line Description</td>
-<td colspan=2><input type=text name=one_line_description size=60></td>
-</tr>
-<tr>
-<td>Additional Descriptive Text</td>
-<td colspan=2><textarea wrap rows=6 cols=60 name=detailed_description></textarea></td>
-</tr>
-<tr>
-<td><p>Search Keywords</p>
-<p>Data from Product's Name, One Line Description, and Other Detailed Description 
-are automatically included in product searches. 
-No need to repeat as keywords.</p></td>
-<td colspan=2><textarea wrap rows=2 cols=60 name=search_keywords></textarea></td>
-</tr>
-<tr>
-<td>Picture</td>
-<td><input type=file size=10 name=upload_file></td>
-<td>This picture (.gif or .jpg format) can be as large as you like.  A thumbnail will be automatically generated.  Note that file uploading doesn't work with Internet Explorer 3.0.</td>
-</tr>
-<tr>
-<td>Color Choices</td>
-<td><input type=text name=color_list size=40></td>
-<td>This should be a comma-separated list of colors the user is allowed to choose from
-when ordering.  If there are no choices, leave this blank.</td>
-</tr>
-<tr>
-<td>Size Choices</td>
-<td><input type=text name=size_list size=40></td>
-<td>This should be a comma-separated list of sizes the user is allowed to choose from
-when ordering.  If there are no choices, leave this blank.</td>
-</tr>
-<tr>
-<td>Style Choices</td>
-<td><input type=text name=style_list size=40></td>
-<td>This should be a comma-separated list of styles the user is allowed to choose from
-when ordering.  If there are no choices, leave this blank.</td>
-</tr>
-<tr>
-<td>Email on Purchase</td>
-<td><input type=text name=email_on_purchase_list size=40></td>
-<td>This should be a comma-separated list of recipients to notify when a purchase is made. If you do not wish email to be sent, leave this blank.</td>
-</tr>
-<tr>
-<td>URL where the consumer can get more info on the product</td>
-<td colspan=2><input type=text name=url size=50 value=\"http://\"></td>
-</tr>
-"
-if { !$multiple_retailers_p } {
-    doc_body_append "<tr>
-    <td>Regular Price</td>
-    <td><input type=text size=6 name=price></td>
-    <td>All prices are in [ad_parameter -package_id [ec_id] Currency ecommerce].  The price should
-    be written as a decimal number (no special characters like \$).
-    </tr>
-    "
-} else {
-    doc_body_append "[ec_hidden_input price ""]\n"
-}
-doc_body_append "<tr>
-<td>Is this product shippable?</td>
-<td><input type=radio name=no_shipping_avail_p value=\"f\" checked>Yes
-&nbsp;&nbsp;
-<input type=radio name=no_shipping_avail_p value=\"t\">No
-</td>
-<td>You might choose \"No\" if this product is actually a service.</td>
-</tr>
-<tr>
-<td>Should this product be displayed when the user does a search?</td>
-<td><input type=radio name=present_p value=\"t\" checked>Yes
-&nbsp;&nbsp;
-<input type=radio name=present_p value=\"f\">No
-</td>
-<td>You might choose \"No\" if this product is part of a series.</td>
-</tr>
-<tr>
-<td>When does this product become available for purchase?</td>
-<td>[ad_dateentrywidget available_date]</td>
-</tr>
-"
-if { !$multiple_retailers_p } {
-    doc_body_append "<tr>
-    <td>Shipping Price</td>
-    <td><input type=text size=6 name=shipping></td>
-    <td rowspan=3 valign=top>The \"Shipping Price\", \"Shipping Price - Additional\", and \"Weight\" fields
-    may or may not be applicable, depending on the 
-    <a href=\"../shipping-costs/index\">shipping rules</a> you have set up for
-    your ecommerce system.</td>
-    </tr>
-    <tr>
-    <td>Shipping Price - Additional per item if ordering more than 1 (leave blank if same as Shipping Price above)</td>
-    <td><input type=text size=6 name=shipping_additional></td>
-    </tr>
-    "
-} else {
-    doc_body_append "[ec_hidden_input shipping ""]\n[ec_hidden_input shipping_additional ""]\n"
-}
-doc_body_append "<tr>
-<td>Weight ([ad_parameter -package_id [ec_id] WeightUnits ecommerce])</td>
-<td><input type=text size=3 name=weight></td>
-</tr>
-</table>
-
-<p>
-"
-
+    set stock_status_html [ec_stock_status_widget]
+    set price_html "" 
+} 
+set available_date_html [ad_dateentrywidget available_date]
+set product_category_html [ec_category_widget t]
 set n_user_classes [db_string num_user_classes_select "select count(*) from ec_user_classes"]
 if { $n_user_classes > 0 && !$multiple_retailers_p} {
-    doc_body_append "<h3>Special Prices for User Classes</h3>
-    
-    <p>
-
-    <table noborder>
-    "
-    
     set first_class_p 1
+    set user_classes_html ""
+
     db_foreach user_class_select "
     select user_class_id, 
            user_class_name 
     from ec_user_classes 
     order by user_class_name" {
-	doc_body_append "<tr><td>$user_class_name</td>
-	<td><input type=text name=\"user_class_prices.$user_class_id\" size=6></td>
-	"
+	append user_classes_html "<tr><td>$user_class_name</td>
+	<td><input type=text name=\"user_class_prices.$user_class_id\" size=6></td>"
 
-	if { $first_class_p } {
-	    set first_class_p 0
-	    doc_body_append "<td valign=top rowspan=$n_user_classes>Enter prices (no
+        if { $first_class_p } {
+            set first_class_p 0
+            append user_classes_html "<td valign=top rowspan=$n_user_classes>Enter prices (no
 	    special characters like \$) only if you want people in
 	    user classes to be charged a different price than the
 	    regular price.  If you leave user class prices blank,
 	    then the users will be charged regular price.</td>\n"
-	}
-	doc_body_append "</tr>\n"
+        }
+        append user_classes_html "</tr>\n"
     }
-    doc_body_append "</table>\n"
 }
 
-if { [db_string num_custom_product_fields_select "select count(*) from ec_custom_product_fields where active_p='t'"] > 0 } {
- 
-    doc_body_append "<h3>Custom Fields</h3>
-    
-    <p>
-
-    <table noborder>
-    "
-    
-    db_foreach custom_fields_select "
+set n_custom_product_fields [db_string num_custom_product_fields_select "select count(*) from ec_custom_product_fields where active_p='t'"]
+if { $n_custom_product_fields > 0 } {
+    set custom_product_fields_html ""
+     db_foreach custom_fields_select "
     select field_identifier, 
            field_name, 
            default_value, 
            column_type 
     from ec_custom_product_fields 
     where active_p='t' order by creation_date" {
-	doc_body_append "<tr><td>$field_name</td><td>[ec_custom_product_field_form_element $field_identifier $column_type $default_value]</td></tr>\n"
+	append custom_product_fields_html "<tr><td>$field_name</td><td>[ec_custom_product_field_form_element $field_identifier $column_type $default_value]</td></tr>\n"
     }
-    doc_body_append "</table>\n"
-
 }
 
-doc_body_append "<center>
-<input type=submit value=\"Continue\">
-</center>
-</form>
-[ad_admin_footer]
-"
