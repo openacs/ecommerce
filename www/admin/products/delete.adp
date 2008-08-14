6af304a32dada8e84b2e64c96e1f93d3deb3bedd
Torben Brosten <torben@dekka.net>
2008-08-14 21:53:58 +0000

    updating to tcl/adp templating

---
+++ www/admin/products/delete.adp
@@ -0,0 +1,17 @@
+<master>
+  <property name="doc(title)">@title;noquote@</property>
+  <property name="context">@context;noquote@</property>
+<h2>@title@</h2>
+<p>
+Are you sure you want to <b>delete $product_name?</b>
+</p>
+<p>The system will not let you delete a product if anyone has already ordered it
+(you might want to mark the product "discontinued" instead).
+</p>
+<p>
+<form method=post action=delete-2>
+@export_vars_html;noquote@
+<center>
+<input type=submit value="Yes">
+</center>
+</form>
--- www/admin/products/delete.tcl
+++ www/admin/products/delete.tcl
@@ -14,25 +14,7 @@ ad_require_permission [ad_conn package_id] admin
 
 set product_name [ec_product_name $product_id]
 
-doc_body_append "[ad_admin_header "Confirm Deletion of $product_name"]
+set title "Confirm Deletion of $product_name"
+set context [list [list index Products] $title]
 
-<h2>Confirm Deletion</h2>
-
-[ad_context_bar [list "../" "Ecommerce([ec_system_name])"] [list "index.tcl" "Products"] [list "one.tcl?[export_url_vars product_id]" "One"] "Confirm Deletion"]
-
-<hr>
-
-Are you sure you want to delete $product_name?  Note that the system
-will not let you delete a product if anyone has already ordered it
-(you might want to mark the product \"discontinued\" instead).
-
-<p>
-<form method=post action=delete-2>
-[export_form_vars product_id]
-<center>
-<input type=submit value=\"Yes\">
-</center>
-</form>
-
-[ad_admin_footer]
-"
+set export_vars_html [export_form_vars product_id]
