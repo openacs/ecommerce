15b9a191234ea0a66db5915dbbd2e63de21ded49
Torben Brosten <torben@dekka.net>
2008-08-18 22:44:44 +0000

    updating templating to tcl/adp

---
+++ www/admin/templates/index.adp
@@ -0,0 +1,14 @@
+<master>
+  <property name="doc(title)">@title;noquote@</property>
+  <property name="context">@context;noquote@</property>
+<h2>@title@</h2>
+
+<ul>
+  @templates_info_html;noquote@
+</ul>
+<br>
+<h3>Actions</h3>
+<ul>
+  <li><a href="@audit_url_html;noquote@">Audit All Templates</a></li>
+  <li><a href="add">Add new template from scratch</a></li>
+</ul>
--- www/admin/templates/index.tcl
+++ www/admin/templates/index.tcl
@@ -10,31 +10,16 @@ ad_page_contract {
 
 ad_require_permission [ad_conn package_id] admin
 
-set page_html "[ad_admin_header "Product Templates"]
+set title "Product Templates"
+set context [list $title]
 
-<h2>Product Templates</h2>
-
-[ad_context_bar [list "../" "Ecommerce([ec_system_name])"] "Product Templates"]
-
-<hr>
-<ul>
-"
-
-#
 # A list of templates and their associated categories (if any)
-#
 
 set the_template_name ""
 set the_template_id  ""
 set the_categories   ""
-proc maybe_write_a_template_line {} {
-    uplevel {
-	if [empty_string_p $the_template_name] { return }
-	append page_html "<li><a href=\"one?template_id=$the_template_id\">$the_template_name</a> \n"
-	regsub {, $} $the_categories {} the_categories
-	if ![empty_string_p $the_categories] { append page_html "<br>associated with categories ($the_categories)" }
-    }
-}
+set templates_info_html ""
+
 db_foreach get_templates_info "
 SELECT t.template_id, t.template_name, c.category_id, c.category_name
   FROM ec_templates t, ec_category_template_map m, ec_categories c
@@ -42,40 +27,39 @@ SELECT t.template_id, t.template_name, c.category_id, c.category_name
    and m.category_id = c.category_id (+)
   ORDER BY template_name, category_name" {
 
-    if {[string compare $template_name $the_template_name] != 0} {
-	maybe_write_a_template_line
+    if { [string compare $template_name $the_template_name] != 0 } {
+        # 	maybe_write_a_template_line
+        if { ![empty_string_p $the_template_name] } {
+            append templates_info_html "<li><a href=\"one?template_id=$the_template_id\">$the_template_name</a> "
+            regsub {, $} $the_categories {} the_categories
+            if { ![empty_string_p $the_categories] } { 
+                append templates_info_html "<br>associated with categories ($the_categories)</li>"
+            } else {
+                append templates_info_html "</li>"
+            }
+        }
+    }
+
 	set the_template_name $template_name
 	set the_template_id   $template_id
 	set the_categories ""
-    }
-    if ![empty_string_p $category_name] {
-	append the_categories "<a href=\"../cat/category?[export_url_vars category_id category_name]\">$category_name</a>, "
+}
+if { ![empty_string_p $category_name] } {
+     append the_categories "<a href=\"../cat/category?[export_url_vars category_id category_name]\">$category_name</a>, "
+}
+
+# maybe_write_a_template_line
+if { ![empty_string_p $the_template_name] } {
+    append templates_info_html "<li><a href=\"one?template_id=$the_template_id\">$the_template_name</a> "
+    regsub {, $} $the_categories {} the_categories
+    if { ![empty_string_p $the_categories] } { 
+        append templates_info_html "<br>associated with categories ($the_categories)</li>"
+    } else {
+        append templates_info_html "</li>"
     }
 }
-maybe_write_a_template_line
 
 # For audit tables
 set table_names_and_id_column [list ec_templates ec_templates_audit template_id]
+set audit_url_html "[ec_url_concat [ec_url] /admin]/audit-tables?[export_url_vars table_names_and_id_column]"
 
-append page_html "
-</ul>
-
-<p>
-
-<h3>Actions</h3>
-
-<ul>
-
-<li><a href=\"[ec_url_concat [ec_url] /admin]/audit-tables?[export_url_vars table_names_and_id_column]\">Audit All Templates</a>
-
-<p>
-
-<li><a href=\"add\">Add new template from scratch</a>
-</ul>
-[ad_admin_footer]
-"
-
-
-
-
-doc_return  200 text/html $page_html
