332a101d4f0b1c32d5568f6a5a02f7b063290a62
Torben Brosten <torben@dekka.net>
2008-08-20 22:32:42 +0000

    updating templating to tcl/adp

---
+++ www/admin/customer-service/gift-certificate-add.adp
@@ -0,0 +1,14 @@
+<master>
+  <property name="doc(title)">@title;noquote@</property>
+  <property name="context">@context;noquote@</property>
+<h2>@title@</h2>
+
+<p>Please confirm that you wish to add @price_html;noquote@ to
+@user_link;noquote@'s gift certificate account (expires $expiration_to_print).</p>
+
+<form method=post action=gift-certificate-add-2>
+ @export_form_vars_html;noquote@
+ <center>
+  <input type=submit value="Confirm">
+ </center>
+</form>
--- www/admin/customer-service/gift-certificate-add.tcl
+++ www/admin/customer-service/gift-certificate-add.tcl
@@ -19,47 +19,20 @@ ad_require_permission [ad_conn package_id] admin
 
 # make sure there's an amount
 if { ![info exists amount] || [empty_string_p $amount] } {
-    ad_return_complaint 1 "<li>You forgot to specify an amount."
+    ad_return_complaint 1 "<li>No gift certificate value specified.</li>"
     return
 }
 
-
-
-set page_title "Confirm New Gift Certificate"
-append doc_body "[ad_admin_header $page_title]
-<h2>$page_title</h2>
-
-[ad_context_bar [list "../index.tcl" "Ecommerce([ec_system_name])"] [list "index.tcl" "Customer Service Administration"] $page_title]
-
-<hr>
-"
-
+set title  "Confirm New Gift Certificate"
+set context [list [list index "Customer Service"] $title]
 
 set expiration_to_print [db_string get_exipre_from_Db "select [ec_decode $expires "" "null" $expires] from dual"]
 set expiration_to_print [ec_decode $expiration_to_print "" "never" [util_AnsiDatetoPrettyDate $expiration_to_print]]
 
-append doc_body "Please confirm that you wish to add [ec_pretty_price $amount [ad_parameter -package_id [ec_id] Currency ecommerce]] to
-<a href=\"[ec_acs_admin_url]users/one?user_id=$user_id\">[db_string get_full_name "select first_names || ' ' || last_name from cc_users where user_id=:user_id"]</a>'s gift certificate account (expires $expiration_to_print).
+set currency [parameter::get -package_id [ec_id] -parameter Currency -default "USD"]
+set price_html [ec_pretty_price $amount $currency]
 
-<p>
+set user_link "<a href=\"[ec_acs_admin_url]users/one?user_id=$user_id\">[db_string get_full_name "select first_names || ' ' || last_name from cc_users where user_id=:user_id"]</a>"
 
-<form method=post action=gift-certificate-add-2>
-[export_form_vars user_id amount expires]
-<center>
-<input type=submit value=\"Confirm\">
-</center>
-
-</form>
-
-[ad_admin_footer]
-"
-
-
-
-doc_return  200 text/html $doc_body
-
-
-
-
-
+set export_form_vars_html [export_form_vars user_id amount expires]
 
