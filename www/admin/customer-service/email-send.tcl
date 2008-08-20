d8688f3ecfc48744e99a7034b54bc82ce0831290
Torben Brosten <torben@dekka.net>
2008-08-20 22:24:52 +0000

    email-send.adp

--- www/admin/customer-service/email-send.tcl
+++ www/admin/customer-service/email-send.tcl
@@ -25,25 +25,15 @@ if {$customer_service_rep == 0} {
     ad_script_abort
 }
 
-
-set page_title "Send Email to Customer"
-append doc_body "[ad_admin_header $page_title]
-<h2>$page_title</h2>
-
-[ad_context_bar [list "../index.tcl" "Ecommerce([ec_system_name])"] [list "index.tcl" "Customer Service Administration"] $page_title]
-
-<hr>
-"
+set title  "Send Email to Customer"
+set context [list [list index "Customer Service"] $title]
 
 # make sure this user_identification_id has an email address associated with it
-
 db_1row get_user_information "
 select u.email as user_email, id.email as id_email
   from cc_users u, ec_user_identification id
  where id.user_id = u.user_id(+)
-   and id.user_identification_id=:user_identification_id
-"
-
+   and id.user_identification_id=:user_identification_id"
 
 if { ![empty_string_p $user_email] } {
     set email_to_use $user_email
@@ -52,66 +42,17 @@ if { ![empty_string_p $user_email] } {
 }
 
 if { [empty_string_p $email_to_use] } {
-    append doc_body "
-    
-    Sorry, we don't have the customer's email address on file.
-    
-    [ad_admin_footer]
-    "
-    doc_return  200 text/html $doc_body
-    ad_script_abort
+    set early_message "No email sent. Sorry, we don't have the customer's email address on file."
+} else {
+   set early_message "If you are not [db_string get_full_name "select first_names || ' ' || last_name from cc_users where user_id=:customer_service_rep"], please <a href=\"/register?[export_url_vars return_url]\">log in</a>"
+
+    # generate action_id here for double-click protection
+    set action_id [db_nextval ec_action_id_sequence]
+
+    set export_form_vars_html "[ec_hidden_input var_to_spellcheck "message"]\
+        [ec_hidden_input target_url "[ec_url_concat [ec_url] /admin]/customer-service/email-send-2.tcl"]\
+        [export_form_vars email_to_use action_id issue_id customer_service_rep user_identification_id]"
+
+    set customer_service_email [parameter::get -package_id [ec_id] -parameter CustomerServiceEmailAddress]
+    set email_form_message [ec_canned_response_selector email_form message]
 }
-
-
-# generate action_id here for double-click protection
-set action_id [db_nextval ec_action_id_sequence]
-
-append doc_body "If you are not [db_string get_full_name "select first_names || ' ' || last_name from cc_users where user_id=:customer_service_rep"], please <a href=\"/register?[export_url_vars return_url]\">log in</a>
-
-<form name=email_form method=post action=../tools/spell>
-[ec_hidden_input var_to_spellcheck "message"]
-[ec_hidden_input target_url "[ec_url_concat [ec_url] /admin]/customer-service/email-send-2.tcl"]
-[export_form_vars email_to_use action_id issue_id customer_service_rep user_identification_id]
-<table>
-<tr>
-<td align=right><b>From</td>
-<td>[ad_parameter -package_id [ec_id] CustomerServiceEmailAddress ecommerce]</td>
-</tr>
-<tr>
-<td align=right><b>To</td>
-<td>$email_to_use</td>
-</tr>
-<tr>
-<td align=right><b>Cc</td>
-<td><input type=text name=cc_to size=30></td>
-</tr>
-<tr>
-<td align=right><b>Bcc</td>
-<td><input type=text name=bcc_to size=30></td>
-</tr>
-<tr>
-<td align=right><b>Subject</td>
-<td><input type=text name=subject size=30></td>
-</tr>
-<tr>
-<td align=right><b>Message</td>
-<td><textarea wrap name=message rows=10 cols=50></textarea></td>
-</tr>
-<tr>
-<td align=right><b>Canned Responses</td>
-<td>[ec_canned_response_selector email_form message]</td>
-</tr>
-</table>
-
-<p>
-
-<center>
-<input type=submit value=\"Send\">
-</center>
-
-</form>
-[ad_admin_footer]
-"
-
-doc_return  200 text/html $doc_body
-
