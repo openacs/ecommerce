fba0f86d783be328737e87a9e3a6f7ef6a25e637
Torben Brosten <torben@dekka.net>
2008-08-25 13:32:01 +0000

    updating templating to tcl/adp

---
+++ www/admin/customer-service/canned-response-delete.adp
@@ -0,0 +1,14 @@
+<master>
+  <property name="doc(title)">@title;noquote@</property>
+  <property name="context">@context;noquote@</property>
+<h2>@title@</h2>
+
+<p>Are you sure you want to delete this prepared response?</p>
+
+<h3>@one_line@</h3>
+@response_html;noquote@
+
+
+<center>
+<p> <a href="canned-response-delete-2?response_id=@response_id@">Yes, get rid of it</a>.</p>
+</center>
--- www/admin/customer-service/canned-response-delete.tcl
+++ www/admin/customer-service/canned-response-delete.tcl
@@ -17,25 +17,7 @@ db_1row get_response_info "select one_line, response_text
 from ec_canned_responses
 where response_id = :response_id"
 
+set title  "Confirm Delete"
+set context [list [list index "Customer Service"] $title]
 
-
-
-doc_return  200 text/html "[ad_admin_header "Confirm Delete"]
-
-<h2>Confirm Delete</h2>
-
-<hr>
-
-Are you sure you want to delete this canned response?
-
-<h3>$one_line</h3>
-[ec_display_as_html $response_text]
-
-<p>
-
-<center>
-<a href=\"canned-response-delete-2?response_id=$response_id\">Yes, get rid of it</a>
-</center>
-
-[ad_admin_footer]
-"
+set response_html [ec_display_as_html $response_text]
