5110f36af78d665f81ff97853fcbfab8771c43f5
Torben Brosten <torben@dekka.net>
2008-09-16 13:42:53 +0000

    adding ec_show_supporting_files proc and a couple of minor code upgrades

--- tcl/ecommerce-widgets-procs.tcl
+++ tcl/ecommerce-widgets-procs.tcl
@@ -691,21 +691,17 @@ ad_proc ec_creditcard_widget {
 } { 
     Credit card selector 
 } {
-    set payment_gateway [ad_parameter -package_id [apm_package_id_from_key "ecommerce"] PaymentGateway]
-    if {[acs_sc_binding_exists_p "PaymentGateway" $payment_gateway]} {
-
-	# Use the cards accepted by ecommerce package or if none have
-	# been specified (in the CreditCardsAccepted parameter) use
-	# the cards accepted by the payment gateway implementation.
-
-	array set info [acs_sc_call PaymentGateway Info [list] $payment_gateway]
-	set accepted_credit_cards $info(cards_accepted)
+    set payment_gateway [parameter::get -package_id [apm_package_id_from_key "ecommerce"] -parameter PaymentGateway]
+    if {[acs_sc_binding_exists_p "PaymentGateway" ${payment_gateway}]} {
+        # Use the cards accepted by ecommerce package or if none have
+        # been specified (in the CreditCardsAccepted parameter) use
+        # the cards accepted by the payment gateway implementation.
+        array set info [acs_sc_call PaymentGateway Info [list] ${payment_gateway}]
+        set accepted_credit_cards $info(cards_accepted)
     } else {
-
-	# There is no binding to a payment gateway, thus use the cards
-	# accepted by the ecommerce package.
-
-	set accepted_credit_cards [ad_parameter -package_id [apm_package_id_from_key "ecommerce"] CreditCardsAccepted]
+        # There is no binding to a payment gateway, thus use the cards
+        # accepted by the ecommerce package.
+        set accepted_credit_cards [parameter::get -package_id [apm_package_id_from_key "ecommerce"] -parameter CreditCardsAccepted]
     }
     set to_return "
 	<select name=\"creditcard_type\""
@@ -1196,4 +1192,65 @@ ad_proc ec_state_widget { {default ""} {select_name "usps_abbrev"} } "Returns a 
     }
     append widget_value "</select>\n"
     return $widget_value
+}
+
+ad_proc ec_show_supporting_files {
+    product_id
+} {
+    shows supporting product files , blank if there are none
+} {
+    set product_files_list ""
+    set file_count 0
+    set pretty_files ""
+    set product_files ""
+    set adobe_reader_blurb 0
+    set dirname [db_string dirname_select "select dirname from ec_products where product_id=$product_id"]
+
+    # make sure there are no /'s in dirname
+    if { [regexp {/} $dirname] } {
+        ns_log Error "Invalid dirname: $dirname"
+    }
+
+    if { ![empty_string_p $dirname] } {
+        set subdirectory [ec_product_file_directory $product_id]
+
+        set full_dirname "[ec_data_directory][ec_product_directory]$subdirectory/$dirname"
+
+        # see what's in that directory
+        set file_list [glob -nocomplain -directory $full_dirname *]
+  
+        foreach file_name $file_list {
+            set file_size [file size $file_name] 
+            set file_size_kb [format "%6.1f" [expr { $file_size / 1024 }]]
+            set file [lindex [file split $file_name] end]
+            if { [string match {product.[jg][pi][gf]} $file] } {
+                # do not show it
+            } elseif { [string match {product-thumbnail.jpg} $file] } {
+                # do not show it
+            } elseif { [string match {product-productimage.jpg} $file] } {
+                # do not show it
+            } else {
+                append product_files "<li><a href=\"[ec_url]product-file/$subdirectory/$dirname/$file\">$file</a> (${file_size_kb}kb "
+                incr file_count
+                if { [string match -nocase {*.pdf} $file] } {
+                     append product_files "Acrobat file"
+                     set adobe_reader_blurb 1
+
+                }
+
+                append product_files ")</li>"
+            }
+        }
+    }
+
+    if { $file_count > 1 } {
+        set pretty_files "s"
+    }
+    if { [string length ${product_files}] > 0 } {
+        set product_files_list "<h3>Downloadable product file${pretty_files}</h3><ul>${product_files}</ul>"
+        if { $adobe_reader_blurb eq 1 } {
+            append product_files_list "<p>After downloading, Acrobat (.pdf) files can be viewed and printed using the free Adobe Acrobat Reader. If you do not have it, <a href=\"http://www.adobe.com/prodindex/acrobat/readstep.html\" target=\"_blank\">Get Adobe Acrobat Reader.</p>"
+        }
+    }
+        return $product_files_list
 }
