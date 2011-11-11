5614ac5b6c31993b08b01f2b2caf83cb493dc8cc
Torben Brosten <torben@dekka.net>
2011-11-11 19:40:42 +0000

    updated, tested, works with hostnode mapping, too.

--- tcl/social-networking-procs.tcl
+++ tcl/social-networking-procs.tcl
@@ -12,18 +12,41 @@ ad_proc -public ecds_share_bar {
     returns html fragment, a series of links for sharing a webpage with others using social networking
 } {
     if { $url eq "" } {
-        set url [ad_return_url]
+        set url [ad_return_url -qualified]
+        # x and y paramters are generated in forms where submit button is type "image"
+        # remove gremilin x and y parameters from form posts by GET
+        # ie make urls less ugly when sharing.
+        if { [ad_conn method] != "POST" } {
+            regsub {[\&][x][\=][0-9]+} $url {} url
+            regsub {[\&][y][\=][0-9]+} $url {} url
+        }
     }
     if { $title eq "" } {
         set title [ad_conn instance_name]
-        ns_log Notice "ecds_share_bar: title has no value, substituting with instance_name"
+        ns_log Notice "ecds_share_bar: title apparently for $url has no value, substituting with instance_name"
     }
-    set links_html "<div class=\"shareclass\">Share: 
-<a href=\"http://twitter.com/share?url=${url}&amp;text=${title}\" target=\"_blank\"><img src=\"http://twitter.com/favicon.ico\" alt=\"Tweet\">Tweet</a> 
-<a href=\"http://www.facebook.com/sharer.php?u=${url}&amp;t=${title}\" target=\"_blank\" title=\"Share on Facebook\" rel=\"nofollow\"><img src=\"http://www.facebook.com/favicon.ico\" alt=\"Facebook\">Facebook</a> 
-<a href=\"http://www.stumbleupon.com/submit?url=${url}\" target=\"_blank\" title=\"Share on StumbleUpon\" rel=\"nofollow\"><img src=\"http://www.stumbleupon.com/favicon.ico\" alt=\"StumbleUpon\">StumbleUpon</a> 
-<a href=\"http://www.digg.com/submit?phase=2&amp;url=${url}\" target=\"_blank\" title=\"Share on Digg\" rel=\"nofollow\"><img src=\"http://www.digg.com/favicon.ico\" alt=\"Digg\">Digg</a> 
-<a href=\"http://del.icio.us/post?url=${url}&amp;title=${title}\" target=\"_blank\" title=\"Share on Delicious\" rel=\"nofollow\"><img src=\"http://del.icio.us/favicon.ico\" alt=\"Delicious\">Delicious</a>
-</div>"
+
+    set url [ns_urlencode $url]
+    set title [ns_urlencode $title]
+
+    if { [security::secure_conn_p] } {
+    set links_html "<span class=\"shareclass\">Share: 
+<a href=\"http://twitter.com/share?url=${url}&amp;text=${title}\" target=\"_blank\" title=\"Share on Twitter\" rel=\"nofollow\">Tweet</a> 
+<a href=\"http://www.facebook.com/sharer.php?u=${url}&amp;t=${title}\" target=\"_blank\" title=\"Share on Facebook\" rel=\"nofollow\">Facebook</a> 
+<a href=\"http://www.stumbleupon.com/submit?url=${url}\" target=\"_blank\" title=\"Share on StumbleUpon\" rel=\"nofollow\">StumbleUpon</a> 
+<a href=\"http://www.digg.com/submit?phase=2&amp;url=${url}\" target=\"_blank\" title=\"Share on Digg\" rel=\"nofollow\">Digg</a> 
+<a href=\"http://del.icio.us/post?url=${url}&amp;title=${title}\" target=\"_blank\" title=\"Share on Delicious\" rel=\"nofollow\">Delicious</a>
+</span>"
+
+    } else {
+    set links_html "<span class=\"shareclass\">Share: 
+<a href=\"http://twitter.com/share?url=${url}&amp;text=${title}\" target=\"_blank\" title=\"Share on Twitter\" rel=\"nofollow\"><img src=\"http://twitter.com/favicon.ico\" alt=\"Tweet\" width=\"16\" height=\"16\" border=\"1\" >Tweet</a> 
+<a href=\"http://www.facebook.com/sharer.php?u=${url}&amp;t=${title}\" target=\"_blank\" title=\"Share on Facebook\" rel=\"nofollow\"><img src=\"http://www.facebook.com/favicon.ico\" alt=\"Facebook\" width=\"16\" height=\"16\" border=\"1\">Facebook</a> 
+<a href=\"http://www.stumbleupon.com/submit?url=${url}\" target=\"_blank\" title=\"Share on StumbleUpon\" rel=\"nofollow\"><img src=\"http://www.stumbleupon.com/favicon.ico\" alt=\"StumbleUpon\" width=\"16\" height=\"16\" border=\"1\">StumbleUpon</a> 
+<a href=\"http://www.digg.com/submit?phase=2&amp;url=${url}\" target=\"_blank\" title=\"Share on Digg\" rel=\"nofollow\"><img src=\"http://cdn1.diggstatic.com/img/favicon.a015f25c.ico\" alt=\"Digg\" width=\"16\" height=\"16\" border=\"1\">Digg</a> 
+<a href=\"http://del.icio.us/post?url=${url}&amp;title=${title}\" target=\"_blank\" title=\"Share on Delicious\" rel=\"nofollow\"><img src=\"http://del.icio.us/favicon.ico\" alt=\"Delicious\" width=\"16\" height=\"16\" border=\"1\">Delicious</a>
+</span>"
+
+    }
     return $links_html
 }
